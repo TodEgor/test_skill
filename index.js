b5c35d860f47b7e6395fe874c292af0fec63a22c
etodarenko <etodarenko@yandex-team.ru>
2020-08-05 12:28:08 +0600

    ь

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ module.exports = async (req, res) => {
     const { request, session, version } = await json(req);
 
     res.end(JSON.stringify(
-        {
+        if (request['command'] === 'выключи компьютер') {
+            return {
+            version,
+            session,
+            session_state: null,
+            user_state_update: null,
+            response: {
+                text: 'пришла команда "выключи компьютер"',
+                tts: 'пришла команда выключи компьютер',
+                end_session: true,
+            }
+        };
+        }
+    
+            return {
             version,
             session,
             response: {
-                text: request.original_utterance || 'Hello!',
+                text: 'привет, команда не пришла',
+                tts: 'привет, команда не пришла',
                 end_session: false,
             },
-        }
+            state,
+        };
     ));
 };
