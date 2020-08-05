6c4a30df256c43d97dcb6fdc5ce71de47b43e5fe
etodarenko <etodarenko@yandex-team.ru>
2020-08-05 12:14:22 +0600

    create example

---
+++ index.js
@@ -0,0 +1,17 @@
+const { json } = require('micro');
+
+module.exports = async (req, res) => {
+
+    const { request, session, version } = await json(req);
+
+    res.end(JSON.stringify(
+        {
+            version,
+            session,
+            response: {
+                text: request.original_utterance || 'Hello!',
+                end_session: false,
+            },
+        }
+    ));
+};
