112d5b78dee2b382163df394d6b99cfd4c42663e
etodarenko <etodarenko@yandex-team.ru>
2020-08-05 12:41:50 +0600

    ь

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
-const { json } = require('micro');
+const micro = require('micro');
+const replies = require('./replies');
 
-module.exports = async (req, res) => {
+const server = micro(async (req, res) => {
+    if (req.method !== 'POST') {
+        return 'Server is running';
+      }
 
-    const { request, session, version } = await json(req);
+    const { request, session, state } = await micro.json(req);
+    const sessionState = state && state.session || {};
+    const response = session.new
+    ? replies.welcome()
+    : checkAnswer(sessionState, request.command);
 
-    res.end(JSON.stringify(
-        if (request['command'] === 'выключи компьютер') {
-            return {
-            version,
-            session,
-            session_state: null,
-            user_state_update: null,
-            response: {
-                text: 'пришла команда "выключи компьютер"',
-                tts: 'пришла команда выключи компьютер',
-                end_session: true,
-            }
-        };
-        }
-    
-            return {
-            version,
-            session,
-            response: {
-                text: 'привет, команда не пришла',
-                tts: 'привет, команда не пришла',
-                end_session: false,
-            },
-            state,
-        };
-    ));
-};
+    return {
+        response,
+        session_state: sessionState,
+        version: '1.0'
+      };
+});
+
+function checkAnswer(sessionState, command) {
+    let question = sessionState.question;
+    if (!question) {
+      question = generateQuestion(sessionState);
+      return replies.firstQuestion(question);
+    }
+
+    if (isCorrectAnswer(command)) {
+      if (isCorrectAnswer(question, command)) {
+        question = generateQuestion(sessionState);
+      return replies.correctAnswer(question);
+    }
+
+    if (/сдаюсь/i.test(command)) {
+        const answer = question.number1 + question.number2;
+        question = generateQuestion(sessionState);
+        return replies.capitulate(answer, question);
+    }    
+
+    return replies.incorrectAnswer(question);
+}
+
+function isCorrectAnswer(question, command) {
+  const matches = command.match(/[0-9]+/);
+  const correctAnswer = question.number1 + question.number2;
+  return matches && Number(matches[0]) === correctAnswer;
+}
+
+function generateQuestion(sessionState) {
+  const question = {
+    number1: Math.ceil(Math.random() * 20),
+    number2: Math.ceil(Math.random() * 20),
+  };
+  sessionState.question = question;
+  return question;
+}  
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
